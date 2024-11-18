72e5ba56e3afe66b981372091fbb87804da2c40d
hong-gyeng-rok <68814771+hong-gyeng-rok@users.noreply.github.com>
2024-11-18 11:21:17 +0900

    Create Theme.js

---
+++ Theme.js
@@ -0,0 +1,21 @@
+t.prefs_.set('color-palette-overrides', [
+  '#000000', // black
+  '#006400', // dark green (Comment)
+  '#32CD32', // bright green (String)
+  '#00FA9A', // medium bright green (Keyword)
+  '#7FFF00', // neon green (Function)
+  '#228B22', // forest green (Identifier)
+  '#3CB371', // mint green (Statement)
+  '#00FF7F', // light green (Normal)
+  '#00FF7F', // bright green (Normal continued)
+  '#006400', // dark green (extended)
+  '#32CD32', // bright green (extended)
+  '#00FA9A', // medium bright green (extended)
+  '#7FFF00', // neon green (extended)
+  '#228B22', // forest green (extended)
+  '#3CB371', // mint green (extended)
+  '#FFFFFF'  // white
+]);
+t.prefs_.set('foreground-color', '#00FF7F'); // 기본 텍스트 색상
+t.prefs_.set('background-color', '#000000'); // 배경색 검정
+t.prefs_.set('cursor-color', 'rgba(0, 255, 127, 0.5)'); // 커서색 연한 녹색
